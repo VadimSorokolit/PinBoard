9cb95c2abe0315620293ad7de65309ea3f6e4ce2
VadimSorokolit <macintosh@ukr.net>
2025-07-07 09:53:02 +0300

    Init project

---
+++ PinBoard/PinBoard/MainView/PinBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinBoardApp.swift
+//  PinBoard
+//
+//  Created by Vadim Sorokolit on 07.07.2025.
+//
+    
+import SwiftUI
+
+@main
+struct PinBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
