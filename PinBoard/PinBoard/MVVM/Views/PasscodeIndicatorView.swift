6aadbd3467db40eca3a9f7ee80e757d67fad7951
VadimSorokolit <macintosh@ukr.net>
2025-07-08 20:59:59 +0300

    Implemented PasscodeIndicatorView with wrong-password red circles pin animation

---
+++ PinBoard/PinBoard/MVVM/Views/PasscodeIndicatorView.swift
@@ -0,0 +1,46 @@
+//
+//  PasscodeIndicatorView.swift
+//  PinBoard
+//
+//  Created by Vadim Sorokolit on 08.07.2025.
+//
+    
+import SwiftUI
+
+struct PasscodeIndicatorView: View {
+    @Environment(PinBoardViewModel.self) var viewModel
+    @Environment(\.colorScheme) var colorScheme
+    let isWrongPassword: Bool
+    private var baseColor: Color { .red }
+    private var strokeColor: Color {
+        isWrongPassword
+        ? .red
+        : (colorScheme == .dark ? .white : .black)
+    }
+    
+    var body: some View {
+        HStack(spacing: 32.0) {
+            ForEach(0..<viewModel.passcodeLength, id: \.self) { idx in
+                Circle()
+                    .fill(baseColor.opacity(isWrongPassword ? 1.0 : 0.0))
+                    .background(
+                        
+                        Circle()
+                            .fill(
+                                !isWrongPassword && (viewModel.passcode.count > idx)
+                                ? Color.primary
+                                : Color.clear
+                            )
+                    )
+                    .frame(width: 20.0, height: 20.0)
+                    .overlay(
+                        Circle()
+                            .stroke(strokeColor, lineWidth: 1.0)
+                    )
+                    .animation(.easeOut(duration: 0.5), value: isWrongPassword)
+            }
+        }
+    }
+    
+}
+
