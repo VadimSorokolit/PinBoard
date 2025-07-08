cc3e912aea98d5e1ed79f894d8b59bd2aa8bae9f
VadimSorokolit <macintosh@ukr.net>
2025-07-08 20:50:28 +0300

    Implemented custom keyboard

---
+++ PinBoard/PinBoard/MVVM/Views/NumberPad.swift
@@ -0,0 +1,67 @@
+//
+//  NumberPad.swift
+//  PinBoard
+//
+//  Created by Vadim Sorokolit on 08.07.2025.
+//
+    
+import SwiftUI
+
+struct NumberPad: View {
+    let onAdd: (_ value: Int) -> Void
+    let onRemoveLast: () -> Void
+    let onDissmis: () -> Void
+    private let columns: [GridItem] = Array(repeating: .init(), count: 3)
+    
+    var body: some View {
+        LazyVGrid(columns: columns){
+            ForEach(1 ... 9, id: \.self) { index in
+                Button(action: {
+                    
+                    onAdd(index)
+                }) {
+                    Text("\(index)")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 16.0)
+                        .contentShape(.rect)
+                }
+            }
+            
+            Button(action: {
+                
+                onRemoveLast()
+            }) {
+                Image(systemName: "delete.backward")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 16.0)
+                    .contentShape(.rect)
+            }
+            
+            Button(action: {
+                
+                onAdd(0)
+            }) {
+                Text("0")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 16.0)
+                    .contentShape(.rect)
+            }
+            
+            Button(action: {
+                
+                onDissmis()
+            }) {
+                Image(systemName: "keyboard.chevron.compact.down")
+                    .font(.title)
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 16.0)
+                    .contentShape(.rect)
+            }
+        }
+        .foregroundStyle(.primary)
+    }
+    
+}
