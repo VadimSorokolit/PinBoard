6b1c6b9b4a8a78daddb89808aa7ffc5b234e3873
VadimSorokolit <macintosh@ukr.net>
2025-07-08 21:11:17 +0300

    Created SignIn and SignUp views

---
+++ PinBoard/PinBoard/MVVM/Views/SignInView.swift
@@ -0,0 +1,16 @@
+//
+//  SignInView.swift
+//  PinBoard
+//
+//  Created by Vadim Sorokolit on 08.07.2025.
+//
+    
+import SwiftUI
+
+struct SignInView: View {
+    
+    var body: some View {
+        Text("SignInView")
+    }
+    
+}
---
+++ PinBoard/PinBoard/MVVM/Views/SignUpView.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpView.swift
+//  PinBoard
+//
+//  Created by Vadim Sorokolit on 08.07.2025.
+//
+    
+import SwiftUI
+
+struct SignUpView: View {
+    
+    var body: some View {
+        Text("SignUpView")
+    }
+    
+}
